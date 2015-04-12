c15b20e0a61486dad99a3625df9987e40a07a7d3
Waner Miranda <wanermiranda@gmail.com>
2015-04-12 17:11:19 -0300

    Almost There!

--- quadit.py
+++ quadit.py
@@ -1,4 +1,5 @@
 #!/bin/python
+__author__ = 'gorigan'
 import glob
 import threading
 import time
@@ -9,32 +10,6 @@ from rotate import *
 
 
 norm_factor = 100.00
-
-
-def show_image(img):
-    plt.imshow(img, cmap=plt.cm.Greys_r)
-    plt.show()
-
-
-def image_hist(img):
-    colors_hist = np.zeros((3, 256))
-    for col in img:
-        for cel in col:
-            b_value = cel[0]
-            g_value = cel[1]
-            r_value = cel[2]
-            colors_hist[0][b_value] += 1
-            colors_hist[1][g_value] += 1
-            colors_hist[2][r_value] += 1
-
-    max_b = colors_hist[0].max()
-    max_g = colors_hist[1].max()
-    max_r = colors_hist[2].max()
-
-    colors_hist[0] *= norm_factor / max_b
-    colors_hist[1] *= norm_factor / max_g
-    colors_hist[2] *= norm_factor / max_r
-    return colors_hist
 
 
 class SlideWindow:
@@ -52,25 +27,24 @@ class Difference:
     def __init__(self, histogram=-1.0, pixel=-1.0):
         self.pixel = pixel
         self.histogram = histogram
-        self.value = (pixel * 0.3) + (histogram * 0.7)
 
     def greater(self, target):
 
-        if (self.value > target.get_value()) or (self.pixel == -1):
+        if (self.get_value() > target.get_value()) or (self.pixel == -1):
             return 1
         else:
             return 0
 
     def get_value(self):
-        return self.value
+        return (self.pixel * 0.7) + (self.histogram * 0.3)
 
     def __str__(self):
-        return "Difference (value= " + str(self.value) + ", pixel= " + str(self.pixel) + ", histogram=" + str(
+        return "Difference (value= " + str(self.get_value()) + ", pixel= " + str(self.pixel) + ", histogram=" + str(
             self.histogram) + ")"
 
 
 class WindowSlider(threading.Thread):
-    def __init__(self, slide_window, thread_id, query_bw, query_hist, target_color, target_bw, column, best):
+    def __init__(self, slide_window, thread_id, query_bw, query_hist, target_color, target_bw, column, best, full=True):
         threading.Thread.__init__(self)
         self.threadID = thread_id
         self.query_bw = query_bw
@@ -80,6 +54,7 @@ class WindowSlider(threading.Thread):
         self.column = column
         self.best = best
         self.crop = None
+        self.full = full
         self.slide_window = slide_window
 
     def run(self):
@@ -94,10 +69,12 @@ class WindowSlider(threading.Thread):
             crop_color = self.target_color[calc_y: y, calc_x: x]
             hist = image_hist(crop_color)
 
-            hist_diff = dist_bin(self.query_hist, hist)
+            hist_diff = get_mse(self.query_hist, hist)
             diff = Difference(hist_diff, 0.0)
 
-            pixel_diff = get_square_diff(self.query_bw, crop_bw)
+            pixel_diff = 0.0
+            if self.full:
+                pixel_diff = get_mse(self.query_bw, crop_bw)
             diff.pixel = pixel_diff
 
             if self.best.greater(diff):
@@ -111,21 +88,34 @@ class WindowSlider(threading.Thread):
                 x = target_width_bw
 
 
-def dist_bin(query, target):
-    diff = np.zeros((3, 256))
-    sums = 0.0
-    for row in range(256):
-        diff[0][row] = (query[0][row] - target[0][row]) ** 2
-        diff[1][row] = (query[1][row] - target[1][row]) ** 2
-        diff[2][row] = (query[2][row] - target[2][row]) ** 2
-        sums += diff[0][row] + diff[1][row] + diff[2][row]
-    return sums / (256 * 3)
+def show_image(img):
+    plt.imshow(img, cmap=plt.cm.Greys_r)
+    plt.show()
 
 
-def get_square_diff(query, target):
+def image_hist(img):
+    colors_hist = np.zeros((256, 3))
+    for i in range(0, 3):
+        colors_hist[:256, i] = cv2.calcHist([img], [i], None, [256], [0, 256])[:256, 0]
+        local_max = colors_hist[:256, i].max()
+        colors_hist[:256, i] *= norm_factor / local_max
+    return colors_hist
+
+
+def get_mse(query, target):
     (m, n) = query.shape[:2]
     sums = np.power(np.array(query)-np.array(target), 2).sum()
     return sums / (m * n)
+
+
+def get_bigger_size(query, target, fraction):
+    (target_h, target_w) = target.shape[:2]
+    (query_h, query_w) = query.shape[:2]
+    if query_h > query_w:
+        factor = (fraction * target_h) / query_h
+    else:
+        factor = (fraction * target_w) / query_w
+    return factor
 
 
 class Finder:
@@ -133,11 +123,9 @@ class Finder:
 
     def __init__(self):
         # used to reduce the amount of data treated
-        base_scale = 0.4
-        # step to slide
-        stride = 5
+        base_scale = 1
         max_threads = 4
-        query_index = 5
+        query_index = 2
         rotation_factor = 360
         angle_range = 360 / rotation_factor
 
@@ -165,15 +153,16 @@ class Finder:
                 last_local_diff = rotate_diff
                 last_local_crop = rotate_crop
 
-                for factor_index in range(4, 0, -1):
+                for factor_index in range(4, 1, -1):
                     # Readjusting the query size and the slide window to match X% of the targeted image
                     factor = 0.1 * factor_index
+
                     print "Factor: " + str(factor)
                     query_color = cv2.imread(dataset_query + query_list[query_index - 1])
                     query_bw = cv2.cvtColor(query_color, cv2.COLOR_BGR2GRAY)
                     query_color = rotate_image(query_color, angle * rotation_factor)
-                    query_height, query_width = query_color.shape[:2]
-                    query_base_scale = (target_width * factor) / query_width
+
+                    query_base_scale = get_bigger_size(query_bw, target_bw, factor)
 
                     query_color = cv2.resize(query_color, None, fx=query_base_scale, fy=query_base_scale)
                     query_bw = cv2.resize(query_bw, None, fx=query_base_scale, fy=query_base_scale)
@@ -182,6 +171,8 @@ class Finder:
 
                     query_color_hist = image_hist(query_color)
 
+                    # step to slide
+                    stride = 5
                     slide_window = SlideWindow(query_height_w, query_width_w, stride)
 
                     print "Target Dimensions"
